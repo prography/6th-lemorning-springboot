9355b61c6a3600ee290bdd8a55eacb88c81db33c
SubAkBa <circle5926@gmail.com>
2020-10-12 12:44:29 +0900

    Update & Delete Product

--- src/main/java/com/example/demo/shop/ProductService.java
+++ src/main/java/com/example/demo/shop/ProductService.java
@@ -20,31 +20,48 @@ import java.util.List;
 @RequiredArgsConstructor
 public class ProductService {
 
-	private final ProductRepository productRepository;
+    private final ProductRepository productRepository;
 
-	@Transactional
-	public Product findByName(String name) throws UsernameNotFoundException { // 시큐리티에서 지정한 서비스이기 때문에 이 메소드를 필수로 구현
-		return productRepository.findByName(name);
-	}
+    @Transactional
+    public Product findByName(String name) throws UsernameNotFoundException { // 시큐리티에서 지정한 서비스이기 때문에 이 메소드를 필수로 구현
+        return productRepository.findByName(name);
+    }
 
-	@Transactional
-	public Long save(ProductDto infoDto) {
-		return productRepository.save(Product.builder()
-				.name(infoDto.getName())
-				.categoryName(infoDto.getCategoryName())
-				.imageUrl(infoDto.getImageUrl())
-				.alarmUrl(infoDto.getAlarmUrl())
-				.price(infoDto.getPrice())
-				.build()).getId();
-	}
+    @Transactional
+    public Long save(ProductDto infoDto) {
+        return productRepository.save(Product.builder()
+                .name(infoDto.getName())
+                .categoryName(infoDto.getCategoryName())
+                .imageUrl(infoDto.getImageUrl())
+                .alarmUrl(infoDto.getAlarmUrl())
+                .price(infoDto.getPrice())
+                .build()).getId();
+    }
 
-	@Transactional
-	public List<Product> findAll() {
-		return productRepository.findAll();
-	}
+    @Transactional
+    public List<Product> findAll() {
+        return productRepository.findAll();
+    }
 
-	@Transactional
-	public Product findById(Long id) {
-		return productRepository.findById(id).orElseThrow(EntityNotFoundException::new);
-	}
+    @Transactional
+    public Product findById(Long id) {
+        return productRepository.findById(id).orElseThrow(EntityNotFoundException::new);
+    }
+
+    @Transactional
+    public Long update(Long id, ProductDto infoDto) {
+        Product p = productRepository.findById(id).orElseThrow(EntityNotFoundException::new);
+
+        p.update(infoDto.getName(), infoDto
+                .getCategoryName(), infoDto.getImageUrl(), infoDto.getAlarmUrl(), infoDto.getPrice());
+
+        return id;
+    }
+
+    @Transactional
+    public Long delete(Long id) {
+        productRepository.deleteById(id);
+
+        return id;
+    }
 }
