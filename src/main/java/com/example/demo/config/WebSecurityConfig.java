2fe35879db0b161c837ada7da69c0a0d6bb72799
이창섭 <ventulus95@gmail.com>
2020-10-17 16:16:32 +0900

    local h2 db 연결

--- src/main/java/com/example/demo/config/WebSecurityConfig.java
+++ src/main/java/com/example/demo/config/WebSecurityConfig.java
@@ -52,8 +52,11 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	protected void configure(HttpSecurity httpSecurity) throws Exception {
 		// We don't need CSRF for this example
 		httpSecurity.csrf().disable()
+				.cors().disable()
+				.headers().frameOptions().disable()
+					.and()
 				// dont authenticate this particular request
-				.authorizeRequests().antMatchers("/authenticate","/signup").permitAll().
+				.authorizeRequests().antMatchers("/h2-console/*","/authenticate","/signup").permitAll().
 				// all other requests need to be authenticated
 				anyRequest().authenticated().and().
 				// make sure we use stateless session; session won't be used to
