e228aa4b5347602b0f662c041e22a7ace5be2353
이창섭 <ventulus95@gmail.com>
2020-10-27 12:53:34 +0900

    s3 업로드 Test 코드 작성 - 업로드 되는것 확인.

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,13 +1,27 @@
 package com.example.demo;
 
+import com.example.demo.upload.S3Service;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.mock.web.MockMultipartFile;
+
+import java.io.IOException;
 
 @SpringBootTest
 class DemoApplicationTests {
 
+	@Autowired
+	S3Service s3Service;
+
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	public void  S3_폴더_업로드() throws IOException {
+		MockMultipartFile firstFile = new MockMultipartFile("data", "filename.txt", "text/plain", "some xml".getBytes());
+		s3Service.upload("", firstFile);
 	}
 
 }
