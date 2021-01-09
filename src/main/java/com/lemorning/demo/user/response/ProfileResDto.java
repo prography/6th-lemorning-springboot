370def2c723ba5b66cd8694e95878c21241ea759
youdongkwan <dkyou7@naver.com>
2021-01-09 12:19:15 +0900

    패키지 이동 및 user -> account 정보 옮기는 과정 진행

--- src/main/java/com/lemorning/demo/user/response/ProfileResDto.java
+++ src/main/java/com/lemorning/demo/user/response/ProfileResDto.java
@@ -5,8 +5,10 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 @Builder
+@Slf4j
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
